9daf7d1aa8f8300592793c00206b0d6013657af9
FX Mobile 16 <fxmobile16@FX-Mobile-16.local>
2014-04-04 15:17:53 -0700

    translate3d realized

--- entry.js
+++ entry.js
@@ -4,30 +4,42 @@ var closeness = require('closeness')
 var released = true
 var t0 = null
 var h = window.innerHeight
-var maxDelta = h * .76
+var maxDelta = h * .86
 var minTop = 0 - maxDelta
 var maxTop = 0
 var wasDragging = false
 var dragged = 0;
-var closeEnough = closeness(maxDelta, 5)
+var closeEnough = closeness(-maxDelta, 5)
 var closeEnoughD = closeness(0, 5)
 var top = 0;
+var updown = false;
 
+hammer(document.body).on('dragup dragdown swipeup swipedown', function(evt){
+  evt.gesture.preventDefault()
+})
+
+ui.top.classList.add('swipe')
 hammer(ui.top).on('swipeup', swipeup)
 hammer(ui.top).on('swipedown',swipedown)
-hammer(ui.top).on('release', function(){
+hammer(ui.top).on('release', function(evt){
   if(!wasDragging) return
+  var d = evt.gesture.direction
+  if(d == 'up' && updown) return
+  if(d == 'down' && !updown) return
   wasDragging = false
   var el = this
-  this.classList.add('swipe')
-  this.addEventListener('transitionend', function(evt){
-    this.classList.remove('swipe')
-    top = parseFloat(getCSS(this, 'top'))
-  }) 
-  
-  if(parseFloat(getCSS(el, 'top')) < minTop / 2) el.style.top = minTop + 'px'  
-  else el.style.top = '0px'
-  top = parseFloat(getCSS(el, 'top'))
+  if(evt.gesture.direction == 'down') xxx = top
+  else xxx = -top
+  if(xxx < minTop / 2) {
+    el.style['-webkit-transform'] = 'translate3D(0,'+(minTop)+'px,0)'
+    top = -maxDelta
+    updown = true
+  }
+  else {
+    el.style['-webkit-transform'] = 'translate3D(0,'+(0)+'px,0)'
+    top = 0
+    updown = false 
+  }
 })
 hammer(ui.top).on('dragup', dragup)
 hammer(ui.top).on('dragdown',dragdown)
@@ -35,45 +47,44 @@ hammer(ui.top).on('dragdown',dragdown)
 function dragdown(evt){
   wasDragging = true
   var el = this, t = 0
-  if(closeEnoughD(t = parseFloat(getCSS(this, 'top')))) return
+  if(!updown) return
+  if(closeEnoughD(-top)) return
   else{
     var delta = evt.gesture.deltaY
-    if(Math.abs(delta) > maxDelta) return
+    if(delta > maxDelta) {
+      return
+    }
     else{
-      this.style.top = top + delta + 'px' 
+      top = -maxDelta+delta
+      this.style['-webkit-transform'] = 'translate3D(0,'+(-maxDelta+delta)+'px,0)'
     }
   }
 }
 function dragup(evt){
   wasDragging = true
   var el = this;
-  if(closeEnough(Math.abs(parseFloat(getCSS(this, 'top'))))) return
+  if(updown) return 
+  if(closeEnough(-top)) return
   var delta = evt.gesture.deltaY
   if(Math.abs(delta) > maxDelta) return
   else{
-    this.style.top = delta + 'px'
+    top = -delta
+    this.style['-webkit-transform'] = 'translate3D(0,'+(delta)+'px,0)'
   }
 }
 
 function swipedown(evt){
   wasDragging = false
-  this.classList.add('swipe')
-  this.style.top = '0'
+  this.style['-webkit-transform'] = 'translate3D(0,'+(0)+'px,0)'
+  updown = false
   top = 0
-  this.addEventListener('transitionend', function(evt){
-    this.classList.remove('swipe')
-    top = parseFloat(getCSS(this, 'top'))
-  }) 
 }
 
 function swipeup(evt){
   wasDragging = false
-  this.classList.add('swipe')
-  this.style.top = 0 - maxDelta + 'px' 
-  this.addEventListener('transitionend', function(evt){
-    this.classList.remove('swipe')
-    top = parseFloat(getCSS(this, 'top'))
-  }) 
+  this.style['-webkit-transform'] = 'translate3D(0,'+(-maxDelta)+'px,0)'
+  top = -maxDelta
+  updown = true
 }
 
 function getCSS(el, prop){
